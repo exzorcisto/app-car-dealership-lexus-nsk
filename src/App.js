8e85f7267b1dde1dee01754167119c659f0f6799
exzorcisto <yimov1804@gmail.com>
2025-03-03 19:29:38 +0700

    commit-2-header-v1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import './index.css';
+
 function App() {
   return (
     <div className="App">
-      shop
+      <Header/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Header.css';
+import logo from '../../img/logo.png';
+import { FaAngleDown } from "react-icons/fa";
+
+export default function Header() {
+  return (
+    <header>
+          <ul className='nav-up'>
+            <li><img className='logo' src={logo} alt="logo"/></li>
+            <ul className='nav-up-info'>
+              <li>+7 (383) 246-00-00<FaAngleDown/></li>
+              <li>Лексус - Новосибирск</li>
+            </ul>
+          </ul>
+          <ul className='nav-menu'>
+            <li><a href='#'>Модельный ряд</a></li>
+            <li><a href='#'>Автомобили в наличии</a></li>
+            <li><a href='#'>Мир Lexus</a></li>
+            <li><a href='#'>Контакты</a></li>
+          </ul>
+    </header>
+  )
+}
